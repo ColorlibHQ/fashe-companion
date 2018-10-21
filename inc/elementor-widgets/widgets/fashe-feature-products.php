c297c434d133d0b62babe1f2fdbbad01f42eb9f5
codepixar <akash@spondonit.com>
2018-10-21 14:48:46 +0600

    Fixed product slider issue

--- inc/elementor-widgets/widgets/fashe-feature-products.php
+++ inc/elementor-widgets/widgets/fashe-feature-products.php
@@ -155,6 +155,75 @@ class Fashe_Feature_Products extends Widget_Base {
 
     <?php
 
+        // Custom script load
+        $this->call_custom_scripts();
+
         }
+
+
+        // Call custom scripts
+    public function call_custom_scripts(){
+
+        ?>
+        <script src="<?php echo plugins_url( '../assets/js/slick.min.js', __FILE__ ); ?>"></script> 
+
+        <script>
+
+            (function ($) {
+                // USE STRICT
+                "use strict";
+
+
+                    /*[ Slick2 ]
+                    ===========================================================*/
+                    $('.slick2').slick({
+                        slidesToShow: 4,
+                        slidesToScroll: 4,
+                        infinite: true,
+                        autoplay: false,
+                        autoplaySpeed: 6000,
+                        arrows: true,
+                        appendArrows: $('.wrap-slick2'),
+                        prevArrow:'<button class="arrow-slick2 prev-slick2"><i class="fa  fa-angle-left" aria-hidden="true"></i></button>',
+                        nextArrow:'<button class="arrow-slick2 next-slick2"><i class="fa  fa-angle-right" aria-hidden="true"></i></button>',  
+                        responsive: [
+                            {
+                              breakpoint: 1200,
+                              settings: {
+                                slidesToShow: 4,
+                                slidesToScroll: 4
+                              }
+                            },
+                            {
+                              breakpoint: 992,
+                              settings: {
+                                slidesToShow: 3,
+                                slidesToScroll: 3
+                              }
+                            },
+                            {
+                              breakpoint: 768,
+                              settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 2
+                              }
+                            },
+                            {
+                              breakpoint: 576,
+                              settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1
+                              }
+                            }
+                        ]    
+                    });
+
+
+            })(jQuery);
+
+        </script>
+
+        <?php
+    }
 	
 }
