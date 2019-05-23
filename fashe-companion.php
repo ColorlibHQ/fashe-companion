6f5373e5539247acbe4098c40db132d898e96fd3
codepixar <akash@spondonit.com>
2019-05-23 12:40:30 +0600

    Updated

--- fashe-companion.php
+++ fashe-companion.php
@@ -1,16 +1,35 @@
 <?php
-/*
- * Plugin Name:       Fashe Companion
- * Plugin URI:        https://colorlib.com/wp/themes/fashe/
- * Description:       Fashe Companion is a companion for Fashe theme.
- * Version:           1.2
- * Author:            Colorlib
- * Author URI:        https://colorlib.com/
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       fashe-companion
- * Domain Path:       /languages
- */
+/**
+* Plugin Name:          Fashe Companion
+* Plugin URI:           https://colorlib.com/wp/themes/fashe/
+* Description:          Fashe Companion is a companion for Fashe theme.
+* Version:              1.3
+* Author:               Colorlib
+* Author URI:           https://colorlib.com/
+* Tested up to:         5.1
+* Requires:             4.6 or higher
+* License:              GPLv3 or later
+* License URI:          http://www.gnu.org/licenses/gpl-3.0.html
+* Requires PHP:         5.6
+* Text Domain:          fashe-companion
+* Domain Path:          /languages
+*
+* Copyright 2019 Colorlib support@colorlib.com
+*
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License, version 3, as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 
 if( !defined( 'WPINC' ) ){
--- readme.txt
+++ readme.txt
@@ -1,9 +1,10 @@
 === Plugin Name ===
 Contributors: colorlibplugins, silkalns
 Tags: woocommerce, widgets, plugin, demo, companion, home page, one page, parallax, social, portfolio, projects
-Requires at least: 3.8
+Requires at least: 4.6
 Tested up to: 5.1
-Stable tag: 1.0.0
+Requires PHP: 5.6
+Stable tag: trunk
 License: GPLv3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
